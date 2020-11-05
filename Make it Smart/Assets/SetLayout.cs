75e4492f98cb3eac1e457227570ea668766f9ad9
HarishPradyot <70936222+HarishPradyot@users.noreply.github.com>
2020-11-05 22:27:50 +0530

    Upgrade Panel Base Layout

---
+++ Make it Smart/Assets/SetLayout.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetLayout : MonoBehaviour
+{
+    private Setup setup;
+    void Start()
+    {
+        setup = FindObjectOfType<Setup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
