1e4fd41952e639de2da0ed83f97fd6ca0cabb021
HarishPradyot <70936222+HarishPradyot@users.noreply.github.com>
2020-11-05 22:47:03 +0530

    Upgrade Panel Base Layout

--- Make it Smart/Assets/Scripts/Harish/rayCast.cs
+++ Make it Smart/Assets/Scripts/Harish/rayCast.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq.Expressions;
+﻿using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,44 +9,65 @@ public class rayCast : MonoBehaviour
     [SerializeField] GameObject upgradePanelCanvas;
     [SerializeField] TextMeshProUGUI buildingname;
     [SerializeField] Image panelSprite;
+    [SerializeField] GameObject[] buttons;
     private Sprite upgradeSprite;
-    Setup setup;
-    PauseMenu pause;
+    private Setup setup;
+    private PauseMenu pause;
+    private Boolean isPanelActive;
+    private string currentBuilding;
     void Start()
     {
-        upgradePanelCanvas.SetActive(false);
+        isPanelActive = false;
+        upgradePanelCanvas.SetActive(isPanelActive);
         setup = FindObjectOfType<Setup>();
         pause = FindObjectOfType<PauseMenu>();
+        currentBuilding = "Building";
     }
     void Update()
     {
         if (!pause.isGamePaused())
         {
-            if (Input.GetMouseButtonDown(0))//LeftMouseClick - 1
+            
+            if (Input.GetMouseButtonDown(0))//LeftMouseClick - 0
             {
+                isPanelActive = false;
                 Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
                 RaycastHit2D hitInfo = Physics2D.Raycast(ray.origin, ray.direction);
                 if (hitInfo)
-                {
-                    if (hitInfo.collider.CompareTag("Building"))
+                    if (hitInfo.collider.CompareTag("UpgradeCanvas"))
                     {
-                        string name = hitInfo.collider.name;
-                        Debug.Log(name);
-                        if (setup.sprite.ContainsKey(name))
-                        {
-                            upgradeSprite = setup.sprite[name];
-                            displayPanel(name);
-                        }
-                        else
-                            upgradePanelCanvas.SetActive(false);
+                        isPanelActive = true;
                     }
-                }
-                else
-                    upgradePanelCanvas.SetActive(false);
+                    else if (hitInfo.collider.CompareTag("Building"))
+                    {
+                        currentBuilding = hitInfo.collider.name;
+                        Debug.Log(currentBuilding);
+                        if (setup.sprite.ContainsKey(currentBuilding))
+                        {
+                            isPanelActive = true;
+                            upgradeSprite = setup.sprite[currentBuilding];
+                        }
+                        if (setup.upgradeList.ContainsKey(currentBuilding))
+                            setButtons();
+                    }
             }
+            if (isPanelActive)
+                displayPanel(currentBuilding);
+            else
+                upgradePanelCanvas.SetActive(isPanelActive);
         }
         else
             upgradePanelCanvas.SetActive(false);
+    }
+    void setButtons()
+    {
+        string[] upgrades=setup.upgradeList[currentBuilding];
+        for (int i = 0; i < upgrades.Length; i++)
+            buttons[i].SetActive(true);
+        for (int i = upgrades.Length; i < buttons.Length; i++)
+            buttons[i].SetActive(false);
+        Image upgradeImage = buttons[0].transform.GetChild(0).transform.GetChild(0).GetComponent<Image>();//This can be used to access the image of the UpgradeButtons
+        upgradeImage.sprite = upgradeSprite;
     }
     void displayPanel(string buildingname)
     {
