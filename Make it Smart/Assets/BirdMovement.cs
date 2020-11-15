90dbbdc4543dfae034f3e3783324043b35a9bfd3
Saisrujan!8 <yayisaisrujan@gmail.com>
2020-11-15 14:05:16 +0530

    Added script for birdmovement
    need figure out compile errors

---
+++ Make it Smart/Assets/BirdMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birdmovement : MonoBehaviour
+{
+    private float speed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position.x = transform.position.x + speed;
+        if (transform.position.x >= 210f)
+        {
+            transform.position = new Vector2(-240f, transform.position.y);
+        }
+    }
+}
