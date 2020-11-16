f9f3575c4e8375171bec8e8a5583977bff503998
Saisrujan!8 <yayisaisrujan@gmail.com>
2020-11-16 18:48:27 +0530

    added pedestrians script
    Incomplete

---
+++ Make it Smart/Assets/AnimationSrujan/Woman/Path.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private Transform[] waypoints;
+    [SerializeField]
+    public float moveSpeed = 2f;
+    private int waypointIndex = 0;
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if(waypointIndex<=waypoints.Length-1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+            if(transform.position==waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+
+        }
+    }
+}
