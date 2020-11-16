264bebfd2e9aac8f0a7ffea730c13a53c287caec
HarishPradyot <70936222+HarishPradyot@users.noreply.github.com>
2020-11-16 22:16:15 +0530

    Popup Text and Fixed Bugs
    Do check for bugs if any

--- Make it Smart/Assets/Scripts/Harish/rayCast.cs
+++ Make it Smart/Assets/Scripts/Harish/rayCast.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using TMPro;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -16,6 +18,11 @@ public class rayCast : MonoBehaviour
     private Boolean isPanelActive;
     private string currentBuilding;
     private Upgrade upgrade1;
+
+    [SerializeField] float padding = 8f;
+    private GameObject popup;
+    private Button currentButton;
+    private Color def, chg;
     void Start()
     {
         isPanelActive = false;
@@ -24,6 +31,9 @@ public class rayCast : MonoBehaviour
         pause = FindObjectOfType<PauseMenu>();
         upgrade1 = FindObjectOfType<Upgrade>();
         currentBuilding = "Building";
+
+        def = buttons[0].transform.GetChild(0).GetComponent<Image>().color;
+        chg = new Color(0, 0.90f, 0.30f, 1);
     }
     void Update()
     {
@@ -80,5 +90,27 @@ public class rayCast : MonoBehaviour
     public void upgrade(int index)
     {
         upgrade1.setVar(index, currentBuilding);
+    }
+    public void mouseEnter(int i)
+    {
+        Debug.Log("Mouse Enter Button:"+i);
+        currentButton = buttons[i - 1].transform.GetChild(0).GetComponent<Button>();
+        popup = buttons[i - 1].transform.GetChild(1).gameObject;
+        RectTransform popupTransform = popup.GetComponent<RectTransform>();
+        String upgradeDesc = setup.upgradeList[currentBuilding][i - 1];
+        TextMeshProUGUI popupText=popup.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        popupText.text = upgradeDesc;
+        Vector2 preferredSize = new Vector2(popupText.preferredWidth + padding * 2f, popupText.preferredHeight + padding * 2f);
+        Vector2 anchorPos = new Vector2(0, preferredSize.y/2f);
+        popupTransform.sizeDelta = preferredSize;
+        popupTransform.anchoredPosition = anchorPos;
+        currentButton.GetComponent<Image>().color = chg;
+        popup.SetActive(true);
+    }
+    public void mouseExit(int i)
+    {
+        Debug.Log("Mouse Exit");
+        currentButton.GetComponent<Image>().color = def;
+        popup.SetActive(false);
     }
 }
