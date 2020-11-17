c3cb264af966cf88559567d88dbcb22dc36766a9
GithinGeorge2k1 <cs19b046@iittp.ac.in>
2020-11-17 12:06:52 +0530

    Set Up scene for providing animations

--- Make it Smart/Assets/AnimationSrujan/Woman/Path.cs
+++ Make it Smart/Assets/AnimationSrujan/Woman/Path.cs
@@ -7,7 +7,7 @@ public class Path : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] private Transform[] waypoints;
     [SerializeField] public float moveSpeed = 2f;
-    private Rigidbody2D myRigidbody;
+    //private Rigidbody2D myRigidbody;
     private int waypointIndex = 0;
     private Vector3 velocityVector;
     private Animator anim;
@@ -16,7 +16,7 @@ public class Path : MonoBehaviour
     {
         anim = GetComponent<Animator>();
         transform.position = waypoints[waypointIndex].transform.position;
-        myRigidbody = GetComponent<Rigidbody2D>();
+        //myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -27,33 +27,35 @@ public class Path : MonoBehaviour
 
     private void Move()
     {
-        if(waypointIndex<=waypoints.Length-1)
+        if (waypoints.Length != 0)
         {
-            velocityVector = new Vector3(waypoints[waypointIndex].transform.position.x - transform.position.x, waypoints[waypointIndex].transform.position.y - transform.position.y, 0);
-            UpdateAnimation();
-            transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
-            if(transform.position==waypoints[waypointIndex].transform.position)
+            if (waypointIndex <= waypoints.Length - 1)
             {
-                if(waypointIndex == waypoints.Length-1)
+                velocityVector = new Vector3(waypoints[waypointIndex].transform.position.x - transform.position.x, waypoints[waypointIndex].transform.position.y - transform.position.y, 0);
+                UpdateAnimation();
+                transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+                if (transform.position == waypoints[waypointIndex].transform.position)
                 {
-                    reverse = true;
-                    waypointIndex = waypoints.Length-1;
-                }
-                if(waypointIndex==0)
-                {
-                    reverse = false;
-                    waypointIndex = 0;
-                }
-                if(!reverse)
-                {
-                    waypointIndex += 1;
-                }
-                if(reverse)
-                {
-                    waypointIndex -= 1;
+                    if (waypointIndex == waypoints.Length - 1)
+                    {
+                        reverse = true;
+                        waypointIndex = waypoints.Length - 1;
+                    }
+                    if (waypointIndex == 0)
+                    {
+                        reverse = false;
+                        waypointIndex = 0;
+                    }
+                    if (!reverse)
+                    {
+                        waypointIndex += 1;
+                    }
+                    if (reverse)
+                    {
+                        waypointIndex -= 1;
+                    }
                 }
             }
-
         }
     }
     void UpdateAnimation()
