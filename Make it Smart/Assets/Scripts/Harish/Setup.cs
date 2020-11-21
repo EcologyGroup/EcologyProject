82ec1b23ec7fa91a8800275f222cfcd94f22bd28
sumanp2710 <71067646+sumanp2710@users.noreply.github.com>
2020-11-22 00:23:15 +0530

    icons-industry

--- Make it Smart/Assets/Scripts/Harish/Setup.cs
+++ Make it Smart/Assets/Scripts/Harish/Setup.cs
@@ -59,7 +59,7 @@ public class Setup : MonoBehaviour
         upgradeList.Add("Hospital", new string[] { "Super Specialty Upgrade", "Modernize Medical Equipment", "eHealthcare", "Awareness Camps", "Biomedical Waste Treatment", "Accidents" });
         upgradeList.Add("PoliceStation", new string[] { "Virtual Police Station", "Modernize Equipment", "Public Safety", "Awareness Camps", "Digitalize Police Data", "Cyber Security" });
         upgradeList.Add("Grid", new string[] { "Solar Field", "Electric Charging Station", "Smart Grid", "Windmill" });
-        upgradeList.Add("Industry", new string[] { "", "XXX", "YYY", "ZZZ" });
+        upgradeList.Add("Industry", new string[] { "Internet of Things", "Smart Supply Chain", "Waste Treatment", "Pollution Tower" });
         upgradeList.Add("Office", new string[] { "Infrastructure", "Wireless Networks", "Awareness Camps", "Training Programs", "Startup Incubators", "Online Deliveries" });
         upgradeList.Add("Municpality", new string[] { "Garbage Collection", "Smart Bins", "Sewage System", "Water Treatment","Increase Tax", "Public Transport", "Intelligent transport systems", "Road Maintaince", "Transportation Stations", "Cabs",
                                     "Public Toilets","Pollution Check"});
